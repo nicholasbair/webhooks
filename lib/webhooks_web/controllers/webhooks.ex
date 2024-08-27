7b1522d3f25162785a9ea2a017f4890e884a40b4
nickbair <bairinc@gmail.com>
2024-08-27 17:08:42 -0600

    Use ExNylas main, transform and log webhook struct

--- lib/webhooks_web/controllers/webhooks.ex
+++ lib/webhooks_web/controllers/webhooks.ex
@@ -1,11 +1,16 @@
 defmodule WebhooksWeb.WebhookController do
   use WebhooksWeb, :controller
+  require Logger
 
   def challenge(conn, params) do
     text(conn, params["challenge"])
   end
 
-  def receive_webhook(conn, _params) do
+  def receive_webhook(conn, params) do
+    params
+    |> ExNylas.WebhookNotifications.to_struct!()
+    |> then(&Logger.info(inspect(&1, pretty: true)))
+
     send_resp(conn, 200, "")
   end
 end
